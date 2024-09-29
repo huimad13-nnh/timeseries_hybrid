2ee916196e0c66e626b11088cda3c55ea96d1142
Nguyen Nhat Huy <nhat.huyng13@gmail.com>
2024-09-29 12:13:37 +0700

    first commit

---
+++ rnn_demo.py
@@ -0,0 +1,32 @@
+import numpy as np 
+import tensorflow as tf 
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.layers import SimpleRNN, Dense
+
+text = "This is GeeksforGeeks a software training institute" 
+chars = sorted(list(set(text))) 
+char_to_index = {char: i for i, char in enumerate(chars)}
+index_to_char = {i: char for i, char in enumerate(chars)}
+
+seq_length = 3 
+sequences = [] 
+labels = [] 
+for i in range(len(text) - seq_length):
+    seq = text[i:i+seq_length] 
+    label = text[i+seq_length] 
+    sequences.append([char_to_index[char] for char in seq]) 
+    labels.append(char_to_index[label])
+
+X = np.array(sequences) 
+y = np.array(labels) 
+X_one_hot = tf.one_hot(X, len(chars)) 
+y_one_hot = tf.one_hot(y, len(chars))
+
+model = Sequential() 
+model.add(SimpleRNN(50, input_shape=(seq_length, len(chars)), activation='relu')) 
+model.add(Dense(len(chars), activation='softmax'))
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X_one_hot, y_one_hot, epochs=100)
+
